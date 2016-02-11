a662f02df980abcd52fa0f14975c61ff20cffa5b
farnoosh <student@epicodus.com>
2016-02-10 20:36:15 -0800

    debug the functionality for s as a first letter of the word

--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,27 @@
-describe('myfunction', function() {
-  it("is false for XXX", function() {
-    expect(myfunction(example)).to.equal(false);
+describe('leetspeak', function() {
+
+  it("is true for o to replace with 0", function() {
+    expect(leetspeak("o")).to.equal('0');
   });
 
-  // it("is true for years divisible by 4", function() {
-  //   expect(leapYear(2004)).to.equal(true);
-  // });
-  //
-  // it("is true for years divisible by 100", function() {
-  //   expect(leapYear(1900)).to.equal(false);
-  // });
-  //
-  // it("is true for years divisible by 400", function() {
-  //   expect(leapYear(2000)).to.equal(true);
-  // });
-  //
-  // it("is false for input that are negative", function() {
-  //   expect(leapYear(-2016)).to.equal(false);
-  // });
+  it("is true for i to replace with 1", function() {
+    expect(leetspeak("I")).to.equal('1');
+  });
+
+  it("is true for leetWord to hard to read", function() {
+    expect(leetspeak("leetWord")).to.be.a('string');
+  });
+
+  it("is true for e to replace with 3", function() {
+    expect(leetspeak("e")).to.equal('3');
+  });
+
+  it("is true for E to replace with 3", function() {
+    expect(leetspeak("E")).to.equal('3');
+  });
+
+  it("is true for s to replace with z", function() {
+    expect(leetspeak("s")).to.equal('s');
+  });
+
 });
